281b33e29e9940194805595a09a6b6d6c8876ef4
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-10 11:32:19 +0700

    chuyendoitiente
    chuyendoitiente

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace chuyendoitiente
+{
+    class Program
+    { 
+   public static void doitien(){
+            int vnd,usd=1;
+            vnd = usd * 23000;
+           Console.WriteLine("{0} USD = {1} VND ", usd, vnd);
+            Console.Read();
+
+        }
+        static void Main(string[] args)
+        {
+             int usd;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Nhập số tiền USD muốn chuyển:");
+            usd = Convert.ToInt32(Console.ReadLine());
+           doitien();
+        
+        }
+    }
+}
